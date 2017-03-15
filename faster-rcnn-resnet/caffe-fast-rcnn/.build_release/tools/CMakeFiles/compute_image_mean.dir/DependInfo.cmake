dc5e227bdc0b681d6d2551ad48e98b488d5e2774
Onkar Ganjewar <ganjewaronkar@gmail.com>
2017-03-15 11:27:59 -0700

    Update caffe-fast-rcnn

---
+++ faster-rcnn-resnet/caffe-fast-rcnn/.build_release/tools/CMakeFiles/compute_image_mean.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/student/objectDetection/faster-rcnn-resnet/caffe-fast-rcnn/tools/compute_image_mean.cpp" "/home/student/objectDetection/faster-rcnn-resnet/caffe-fast-rcnn/build/tools/CMakeFiles/compute_image_mean.dir/compute_image_mean.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "CAFFE_VERSION=1.0.0-rc3"
+  "GTEST_USE_OWN_TR1_TUPLE"
+  "USE_LEVELDB"
+  "USE_LMDB"
+  "USE_OPENCV"
+  "WITH_PYTHON_LAYER"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/student/objectDetection/faster-rcnn-resnet/caffe-fast-rcnn/build/src/caffe/CMakeFiles/caffe.dir/DependInfo.cmake"
+  "/home/student/objectDetection/faster-rcnn-resnet/caffe-fast-rcnn/build/src/caffe/CMakeFiles/proto.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "include"
+  "/usr/local/cuda/include"
+  "/usr/include/opencv"
+  "/usr/include/atlas"
+  "/usr/include/python2.7"
+  "/usr/local/lib/python2.7/dist-packages/numpy/core/include"
+  "../include"
+  "."
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
