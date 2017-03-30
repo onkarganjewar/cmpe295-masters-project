de8e8bc48142e700a382649693a0b36fb95a0505
Nimish Unde <nimish.2092@gmail.com>
2017-03-30 07:50:45 -0700

    Create express server

--- objectDetectionServer/server.js
+++ objectDetectionServer/server.js
@@ -1,2 +1,10 @@
 'use strict';
-console.log("This is modified Server");
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+	res.send("<h1>Object Detection Server</h1>");
+});
+
+app.listen(3000, () => console.log("Object detection server running on port 3000"));
