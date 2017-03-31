1b768580fe426ca37ded4398489457b7bffeb6bc
Nimish Unde <nimish.2092@gmail.com>
2017-03-30 17:13:34 -0700

    Add formidable and static views

--- objectDetectionServer/server.js
+++ objectDetectionServer/server.js
@@ -2,9 +2,10 @@
 
 const express = require('express');
 const app = express();
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res, next) => {
 	res.send("<h1>Object Detection Server</h1>");
 });
 
-app.listen(3000, () => console.log("Object detection server running on port 3000"));
+app.listen(app.get('port'), () => console.log("Object detection server running on port 3000"));
